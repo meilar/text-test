673461371ac6d4c21c27d34b8683825ac699c650
Matt Eilar <meilar@gmail.com>
2022-01-19 09:25:45 -0800

    add utility logic section, add empty string checker to utility logic

--- README.md
+++ README.md
@@ -103,6 +103,13 @@ Code:
 `numberOfOccurrencesInText(word, text);`
 Expected Output: 3
 
+Test: "If an empty string is passed in as a word, it should return 0."
+Code:
+const word = "";
+const text = "red RED Red!";
+wordCounter(word, text);
+Expected Output: 0
+
 ## Known Issues
 
 ## Acknowledgements
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
+// Utility Logic
+
+function noInputtedWord(word, text) {
+  return ((text.trim().length === 0) || (word.trim().length === 0));
+}
+
 // Business Logic
 
 function wordCounter(text) {
@@ -6,29 +12,48 @@ function wordCounter(text) {
   }
   let wordCount = 0;
   const wordArray = text.split(" ");
-  wordArray.forEach(function (element) {
+  wordArray.forEach(function(element) {
     if (!Number(element)) {
-      wordCount++
+      wordCount++;
     }
   });
   return wordCount;
-};
+}
 
 function numberOfOccurrencesInText(word, text) {
-  if (text.trim().length === 0) {
+  if (noInputtedWord(word, text)) {
     return 0;
   }
   const wordArray = text.split(" ");
   let wordCount = 0;
   wordArray.forEach(function(element) {
     if (element.toLowerCase().includes(word.toLowerCase())) {
-      wordCount++
+      wordCount++;
     }
   });
   return wordCount;
 }
 
 // UI Logic
+
+function boldPassage(word, text) {
+  if (noInputtedWord(word, text)) {
+    return "";
+  }
+  let htmlString = "<p>";
+  let textArray = text.split(" ");
+  textArray.forEach(function(element, index) {
+    if (element.toLowerCase().includes(word.toLowerCase())) {
+      htmlString = htmlString.concat("<b>" + element + "</b>");
+    } else {
+      htmlString = htmlString.concat(element);
+    }
+    if (index !== (textArray.length - 1)) {
+      htmlString = htmlString.concat(" ");
+    }
+  });
+  return htmlString + "</p>";
+}
 
 $(document).ready(function(){
   $("form#word-counter").submit(function(event){
@@ -39,5 +64,6 @@ $(document).ready(function(){
     const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
     $("#total-count").html(wordCount);
     $("#selected-count").html(occurrencesOfWord);
+    $("#bolded-passage").html(boldPassage(word, passage));
   });
 });
